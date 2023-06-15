75736ca6cd9173332feed191c47fcbb79140ec17
comhendrik <hendrik0807@icloud.com>
2023-06-15 22:07:39 +0200

    experimening with tests

--- FlutterProjects/learn_tdd_ca/lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
+++ FlutterProjects/learn_tdd_ca/lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
@@ -1,9 +1,10 @@
 import 'package:dartz/dartz.dart';
-
 import '../../../../core/error/failure.dart';
 import '../entities/number_trivia.dart';
 
-abstract class NumberTriviaRepository {
-  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number);
-  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia();
+class NumberTriviaRepository {
+  NumberTrivia getConcreteNumberTrivia()  {
+    return const NumberTrivia(text: 'test', number: 1);
+  }
 }
+
