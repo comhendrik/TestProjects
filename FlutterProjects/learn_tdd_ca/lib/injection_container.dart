067d3d7377b3185e992fafbe49221c4ad17c3731
comhendrik <hendrik0807@icloud.com>
2023-07-11 12:48:30 +0200

    part 13 14

---
+++ FlutterProjects/learn_tdd_ca/lib/injection_container.dart
@@ -0,0 +1,50 @@
+import 'package:get_it/get_it.dart';
+import 'package:dio/dio.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
+import 'core/network/network_info.dart';
+import 'core/util/input_converter.dart';
+import 'features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
+import 'features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
+import 'features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+import 'features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  //! Features - Number Trivia
+  // Bloc
+  sl.registerFactory(
+        () => NumberTriviaBloc(
+      getConcreteNumberTrivia: sl(),
+      getRandomNumberTrivia: sl(),
+      inputConverter: sl(),
+    ),
+  );
+
+  // Use cases
+  sl.registerLazySingleton(() => GetConcreteNumberTrivia(sl()));
+  sl.registerLazySingleton(() => GetRandomNumberTrivia(sl()));
+
+  // Repository
+  sl.registerLazySingleton<NumberTriviaRepository>(
+        () => NumberTriviaRepositoryImpl(
+      networkInfo: sl(),
+      remoteDataSource: sl(),
+    ),
+  );
+
+  // Data sources
+  sl.registerLazySingleton<NumberTriviaRemoteDataSource>(
+        () => NumberTriviaRemoteDataSourceImpl(client: sl()),
+  );
+  //! Core
+  sl.registerLazySingleton(() => InputConverter());
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
+
+  //! External
+  sl.registerLazySingleton(() => Dio());
+  sl.registerLazySingleton(() => InternetConnectionChecker());
+}
